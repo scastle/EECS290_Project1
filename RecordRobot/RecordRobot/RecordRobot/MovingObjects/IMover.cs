8339ad1b6d4ce1cd3ac97eccec3661c6ac99cd07
Steffen Castle <smc118@case.edu>
2011-02-17 14:29:06 -0500

    Added Mover class

---
+++ RecordRobot/RecordRobot/RecordRobot/MovingObjects/IMover.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace RecordRobot.MovingObjects
+{
+    class Mover
+    {
+        Point Location;
+
+        public void MoveLeft()
+        {
+        }
+
+        public void MoveRight()
+        {
+        }
+
+        public void MoveUp()
+        {
+        }
+
+        public void MoveDown()
+        {
+        }
+    }
+}
