9aa0ff2273388438fbf3d7cb2a3e97202cdbd9a1
Steffen Castle <smc118@case.edu>
2011-02-18 00:07:10 -0500

    Added Robot and Maze classes

---
+++ RecordRobot/RecordRobot/RecordRobot/MovingObjects/Robot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RecordRobot.MovingObjects
+{
+    public enum Direction 
+    {
+        Up, Down, Left, Right
+    }
+
+    class Robot : Mover
+    {
+        /// <summary>
+        /// The current direction the robot is moving
+        /// </summary>
+        public Direction CurrentDirection;
+
+        /// <summary>
+        /// The direction the robot will go at the next intersection
+        /// </summary>
+        public Direction NextDirection;
+    }
+}
