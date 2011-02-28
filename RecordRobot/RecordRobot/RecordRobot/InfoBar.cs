4339e38be302ba4cef70d2cb47efd9159f0fc05f
Mike O <Michael@.(none)>
2011-02-28 15:53:28 -0500

    Display record order in Info Bar

--- RecordRobot/RecordRobot/RecordRobot/InfoBar.cs
+++ RecordRobot/RecordRobot/RecordRobot/InfoBar.cs
@@ -17,6 +17,7 @@ namespace RecordRobot
         public static Vector2 LivesPosition = new Vector2(0, 450);
         public static Vector2 ScorePosition = new Vector2(150, 450);
         public static Vector2 TimePosition = new Vector2(300, 450);
+        public static Vector2 RecordPosition = new Vector2(400, 450);
 
         public static void Draw()
         {
@@ -26,6 +27,14 @@ namespace RecordRobot
             Game1.spriteBatch.DrawString(Game1.Font, "Lives: " + MovingObjects.MovingObjectManager.RobotPlayer.Lives, LivesPosition, Color.White);
             Game1.spriteBatch.DrawString(Game1.Font, "Score: " + ScoreManager.CurrentScore, ScorePosition, Color.White);
             Game1.spriteBatch.DrawString(Game1.Font, "Time: " + elapsedTime.Minutes + ":" + (elapsedTime.Seconds % 60).ToString("d2"), TimePosition, Color.White);
+            MovingObjects.Mover[] m = MovingObjects.MovingObjectManager.Objects.ToArray();
+            for (int i = 0; i <= Game1.CurrentLevel.NumRecords; i++)
+            {
+                if (m[i] is MovingObjects.Record)
+                {
+                    Game1.spriteBatch.Draw(m[i].Texture, new Vector2(400 + i * 30, 455), Color.White);
+                }
+            }
             Game1.spriteBatch.End();
         }
     }
